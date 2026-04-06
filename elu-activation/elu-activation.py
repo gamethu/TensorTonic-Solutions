755cf655ef684e3d344581d1db698df42794ab8e
ntdai <70790046+gamethu@users.noreply.github.com>
2026-04-06 23:50:45 +0700

    Update solution for elu-activation

--- elu-activation/elu-activation.py
+++ elu-activation/elu-activation.py
@@ -4,4 +4,5 @@ def elu(x, alpha):
     """
     Apply ELU activation to each element.
     """
-    return [i if i > 0 else alpha*(np.exp(i)-1) for i in x]
+    res = [i if i > 0 else alpha*(np.exp(i)-1) for i in x]
+    return res
