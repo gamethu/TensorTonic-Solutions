f3a4319a170d88bca7a6248436c2e62326c38449
ntdai <70790046+gamethu@users.noreply.github.com>
2026-04-06 23:49:19 +0700

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -7,10 +7,10 @@ def matrix_transpose(A):
     rows = len(A)
     cols = len(A[0])
     
-    l = list()
+    res = list()
     for j in range(cols):
-        l.append([None] * rows)
+        res.append([None] * rows)
         for i in range(rows):
-            l[j][i] = A[i][j]
-    return np.asarray(l)
+            res[j][i] = A[i][j]
+    return np.asarray(res)
             
